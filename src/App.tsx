f14b0821986330143870993f232726e35d14a05c
marllon-cesar <marllon.cesar@sblabs.dev>
2024-03-27 01:04:49 -0300

    feat: personal info component changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import '../src/app.css';
 import PersonalInfo from './components/PersonalInfo';
+import { Container } from '@mui/material';
 
 function App() {
   return (
     <>
-      <PersonalInfo />
+      <Container
+        maxWidth={false}
+        sx={{ backgroundColor: 'rgb(15, 23, 42)', my: 0, py: 0 }}
+      >
+        <PersonalInfo />
+      </Container>
     </>
   );
 }
--- src/components/PersonalInfo.tsx
+++ src/components/PersonalInfo.tsx
@@ -1,52 +1,76 @@
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container, IconButton, Typography } from '@mui/material';
 import { EASTMAN, WHITE_TEXT } from '../utils/constants';
+//icons
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
-export default function PersonalInfo() {
+interface PersonalInfoProps {
+  styles?: any;
+}
+
+export default function PersonalInfo(props: PersonalInfoProps) {
+  console.log(props);
   return (
-    <Container
-      maxWidth={false}
-      sx={{ backgroundColor: 'rgb(15, 23, 42)', my: 0, py: 0 }}
-    >
-      <Container>
-        <Box
+    <Container sx={{ ...props.styles }}>
+      <Box
+        sx={{
+          py: 12,
+        }}
+      >
+        <Typography
           sx={{
-            py: 12,
+            color: WHITE_TEXT,
+            fontWeight: '700',
+            fontSize: '3rem',
+            lineHeight: 1,
+            fontFamily: EASTMAN,
           }}
         >
-          <Typography
-            sx={{
-              color: WHITE_TEXT,
-              fontWeight: '700',
-              fontSize: '3rem',
-              lineHeight: 1,
-              fontFamily: EASTMAN,
-            }}
-          >
-            Márllon César
-          </Typography>
-          <Typography
-            sx={{
-              color: WHITE_TEXT,
-              fontFamily: EASTMAN,
-              fontWeight: 600,
-              mt: 2,
-            }}
-          >
-            Júnior Full-Stack Developer
-          </Typography>
-          <Typography
-            sx={{
-              color: WHITE_TEXT,
-              fontFamily: EASTMAN,
-              fontWeight: 500,
-              mt: 2,
-            }}
-          >
-            Eu desenvolvo interfaces intuitivas e <br></br> sistemas de
-            gerenciamento eficazes.
-          </Typography>
-        </Box>
-      </Container>
+          Márllon César
+        </Typography>
+        <Typography
+          sx={{
+            color: WHITE_TEXT,
+            fontFamily: EASTMAN,
+            fontWeight: 600,
+            mt: 2,
+            fontSize: '1.4em',
+          }}
+        >
+          Júnior Full-Stack Developer
+        </Typography>
+        <Typography
+          sx={{
+            color: WHITE_TEXT,
+            fontFamily: EASTMAN,
+            fontWeight: 500,
+            mt: 2,
+            fontSize: '1em',
+            lineHeight: 1.2,
+            textAlign: 'justify',
+          }}
+        >
+          Meu expertise reside na concepção e criação de interfaces <br />{' '}
+          altamente intuitivas, integradas a sistemas de gerenciamento robustos,
+          <br />
+          que proporcionam experiências de usuário excepcionais e simplificam{' '}
+          <br />
+          processos complexos.
+        </Typography>
+      </Box>
+
+      <Box>
+        <IconButton href='https://github.com/marlloncps' target='_blank'>
+          <GitHubIcon sx={{ color: WHITE_TEXT, cursor: 'pointer' }} />
+        </IconButton>
+        <IconButton href='https://instagram.com/marlloncps' target='_blank'>
+          <InstagramIcon sx={{ color: WHITE_TEXT, cursor: 'pointer' }} />
+        </IconButton>
+        <IconButton href='https://linkedin.com/in/marlloncps' target='_blank'>
+          <LinkedInIcon sx={{ color: WHITE_TEXT, cursor: 'pointer' }} />
+        </IconButton>
+      </Box>
     </Container>
   );
 }
